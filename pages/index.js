b54fd7ec4508a2fcad6547c916bd6d4e762fd897
Jonathan Bruno <jonathanbruno97@gmail.com>
2023-09-12 20:29:10 -0500

    updates to header

--- pages/index.js
+++ pages/index.js
@@ -38,11 +38,11 @@ const Home = () => {
       {/* image */}
       <div className='w-[1200px] h-full absolute right-0 bottom-0'>
         {/* bg img */}
-        <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>image</div>
+        <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'></div>
         {/* particles */}
-        <ParticlesContainer />
+        {/* <ParticlesContainer /> */}
         {/* avatar img */}
-        <motion.div variants={fadeIn('up', 0.5)} initial="hidden" animate="show" exit="hidden" transition={{ duration: 1, ease: 'easeInOut' }} className='w-full h-ful; max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]'>
+        <motion.div variants={fadeIn('up', 0.5)} initial="hidden" animate="show" exit="hidden" transition={{ duration: 1, ease: 'easeInOut' }} className='w-full h-full max-w-[737px] max-h-[550px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]'>
           <Avatar />
         </motion.div>
       </div>
